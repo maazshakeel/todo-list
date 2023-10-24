9afea2bf033148a1554bb4527e3f7bb949ddde6d
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-10-24 09:18:06 +0700

    feat(board): add 3 boards - todos, progress, done

---
+++ app/_components/Board.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Board() {
+  return (
+    <div className="flex flex-row gap-4">
+      <div className="h-[35rem] w-96 border-[2px] border-[#7a828e] rounded-md border-solid"></div>
+      <div className="h-[35rem] w-96 border-[2px] border-[#7a828e] rounded-md border-solid"></div>
+      <div className="h-[35rem] w-96 border-[2px] border-[#7a828e] rounded-md border-solid"></div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
+import { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
+import Board from "./_components/Board";
 
 export default function Home() {
+  const [showBoard, setShowBoard] = useState<boolean>(false);
+
   return (
-    <main className="flex min-h-screen w-screen items-center justify-center">
-      <button
-        className="border-dashed border-4 border-gray-900 p-24 rounded-xl"
-        onClick={() => alert("clicked!")}
-      >
-        <AiOutlinePlus size={150} color={"#000"} />
-      </button>
+    <main className="flex min-h-screen w-screen items-center justify-center bg-[#0a0c10]">
+      {!showBoard ? (
+        <button
+          className="border-dashed border-4 border-gray-900 p-24 rounded-xl"
+          onClick={() => setShowBoard(true)}
+        >
+          <AiOutlinePlus size={150} color={"#000"} />
+        </button>
+      ) : (
+        <Board />
+      )}
     </main>
   );
 }
